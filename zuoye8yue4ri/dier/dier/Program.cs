070c99c151858acef468012a944571bc79326ccf
希洛特 <1210079813@qq.com>
2026-08-04 20:37:35 +0800

    8月4日作业

---
+++ zuoye8yue4ri/dier/dier/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net.Sockets;
+
+namespace dier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // list数据集合
+            // 加强版的数组: list列表集合 可以新增 可以删除
+            List<string> zu = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+            // list集合中的数据 也是通过下标访问的
+            //Console.WriteLine(zu[0]);
+            //Console.WriteLine(zu[1]);
+            //Console.WriteLine(zu[2]);
+            //Console.WriteLine(zu[3]);
+            //下标超出范围就报错
+
+            // 修改
+            //zu[0] = "nn";
+            //Console.WriteLine(zu[0]);
+
+            // 获取长度
+            //Console.WriteLine(zu.Count);
+
+            // 不能访问 范围外的下标
+            //zu[5] = "nn"; // 报错
+
+            //增加
+            //zu.Add("F");
+            //Console.WriteLine(zu[5]);
+
+            //增加多个
+            //zu.AddRange(new List<string>() { "GG", "HH" }); //增加多个必须是一个集合
+            //Console.WriteLine(zu[5]);
+
+            //在任意下标位置新增数据
+            //list集合.Insert(插入的下标位置,要插入的数据)
+            //zu.Insert(2, "YY");
+            //Console.WriteLine(zu[2]);
+
+            //将List中指定的数据删除
+            //zu.Remove("A");//直接写数据，而不是顺序，会删除相同的所有数据
+            //Console.WriteLine(zu[0]);
+
+            //将List中指定下标的数据删除
+            //zu.RemoveAt(0);//写的是顺序
+            //Console.WriteLine(zu[0]);
+
+            //删除数组中指定的多个数据
+            //list集合.RemoveRange(下标,个数) // 从下标开始删除指定个数
+            //zu.RemoveRange(0, 3);
+            //Console.WriteLine(zu[0]);
+
+            //清空所有数据
+            //zu.Clear();
+
+            //Contains判断List中是否包含某个数据
+            //Console.WriteLine(zu.Contains("A"));
+
+            //IndexOf：查找某个数据在List中第一次出现的下标，结果找到就是下标，找不到就是-1
+            //Console.WriteLine(zu.IndexOf("A"));
+            //LastIndexOf：找某个数据在List中最后一次出现的下标，找到就得到下标，找不到就是-1
+
+            //从List中获取多个数据
+            // GetRange(开始下标, 个数) 返回一个list集合
+
+            //翻转
+            //zu.Reverse();
+            //Console.WriteLine(zu[0]);
+        }
+    }
+}
---
+++ zuoye8yue4ri/zuoye8yue4/zuoye8yue4/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace zuoye8yue4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //数据字典
+            //Dictionary<键名的类型,键值的类型> 变量名 = new Dictionary<数据类型,数据类型> {初始值}
+            Dictionary<string, dynamic> man = new Dictionary<string, dynamic>()
+            {
+                ["name"] = "小红",
+                ["age"] = 20,
+                ["gender"] = "女",
+                ["friend"] = new Dictionary<string, dynamic>()//键值也可以是个字典
+                {
+                    ["name"] = "小张",
+                    ["age"] = 22,
+                    ["gender"] = "男",
+                }
+            };
+            //访问
+            //Console.WriteLine(man["age"]);//输出键名对应的键值
+            //Console.WriteLine(man["age"].GetType());//输出键值的类型
+            //Console.WriteLine(man["friend"]["name"]);//输出作为字典的键值里的键值
+
+            // 了解: 通过方法获取 字典的键值 
+            // 语法: 字典.TryGetValue(键名, out 类型 接受键值的变量)
+            // 返回值: 布尔值
+            //Console.WriteLine(man.TryGetValue("name", out dynamic a));//先通过这句查找键名，有就定义了a
+            //Console.WriteLine(a);//然后输出a
+
+            // 获取字典中的元素个数  字典.Count
+            //Console.WriteLine(man.Count);
+            //Console.WriteLine(man["friend"].Count);//字典中的字典元素个数
+
+            // 可以修改键值，直接赋值新的就行
+            //man["friend"]["name"] = "流流";
+            //Console.WriteLine(man["friend"]["name"]);
+
+            // 添加 (键名不存在就是添加)
+            //man["height"] = 180;
+            //Console.WriteLine(man["height"]);
+
+            // 删除 
+            //man.Remove("gender");
+            //Console.WriteLine(man.TryGetValue("gender", out dynamic b));//已经删除了键名，也就无法定义b
+
+            // 清空 .Clear()
+            //man.Clear();
+            //Console.WriteLine(man.TryGetValue("name", out dynamic v1));
+            //Console.WriteLine(man.TryGetValue("age", out dynamic v2));//删除了所有，只会返回错误
+
+        }
+    }
+}
