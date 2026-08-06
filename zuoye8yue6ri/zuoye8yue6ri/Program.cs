f862ccf6d5b76f86101772402c32bc77451602ec
希洛特 <1210079813@qq.com>
2026-08-06 20:39:39 +0800

    8月6日作业

---
+++ zuoye8yue6ri/zuoye8yue6ri/Program.cs
@@ -0,0 +1,176 @@
+﻿namespace zuoye8yue6ri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sum = 0;
+            //for(int i = 0; i<=100;i++)
+            //{
+            //    if(i%2==0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+            //100以内的偶数之和
+            /*******************************/
+            //int count = 0;
+            //for (int i = 1000; i <= 2000; i++)
+            //{
+            //    if(i%4==0&&i%100!=0||i%400==0)
+            //    {
+            //        Console.Write($"{i} ");
+            //        count++;
+            //        if (count % 4 == 0)
+            //        {
+            //            Console.WriteLine();
+            //        }
+            //    }
+            //}
+            //显示出1000-2000年中所有的闰年，并以每行四个数的形式输出
+
+
+            /**********************************************/
+
+            //for (int i = 9; i >= 0; i--)
+            //{
+            //    for (int j = 0;j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //打印出倒直角三角形
+
+            /************************************************/
+
+            //double sum = 0;
+            //for (int i = 1; i <= 100; i++) 
+            //{
+            //    double a = i % 2 == 1 ? 1 : -1;
+            //    sum += a*1/i;
+            //}
+            //Console.WriteLine(sum);
+            //循环计算1 - 1/2 + 1/3 - 1/4 + ... - 1/100
+
+            /**********************************************/
+
+            //long a = 0;   // 保存阶乘总和
+
+            //for (int n = 1; n <= 10; n++)
+            //{
+            //    long b = 1;// 保存每个数字的阶乘
+            //    for (int i = 1;i <= n; i++)
+            //    {
+            //        b = b * i;
+            //    }
+            //    a = a + b;
+            //}
+
+            //Console.WriteLine(a);
+
+            /*********************************************/
+            //int i = 0;
+            //double h = 5;
+            //do
+            //{
+            //    h = h * 0.3;
+            //    i++;
+            //} while (h > 0.1);
+            //Console.WriteLine(i);
+
+            /******************************************/
+            //double a = 0;
+            //double b = 1; //第一格的芝麻粒数 2^0
+
+            //for (int i = 1; i <= 64; i++)
+            //{
+            //    a = a + b;
+            //    b = b * 2; //下一格翻倍
+            //}
+            //double n = a * 0.00001;
+            //Console.WriteLine($"总芝麻粒数：{a}");
+            //Console.WriteLine($"总重量(kg)：{n}");
+
+            /****************************************/
+            //int i = 0;
+            //double money = 50000;
+            //do
+            //{
+            //    money = money * 0.95;
+            //    i++;
+            //} while (money > 5000);
+            //Console.WriteLine(i);
+
+            /**************************************/
+            //int tao = 1;
+            //for(int i=1;i<=7;i++)
+            //{
+            //    tao = (tao + 1) * 2;
+            //}
+            //Console.WriteLine(tao);
+
+            /*************************************/
+            //double h = 10;
+            //double luo = 10;
+            //double sum = 0;
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    h = h * 0.5 ;
+            //    sum += h ;
+            //}
+            //sum = sum * 2+luo;
+            //Console.WriteLine(sum);
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+    }
