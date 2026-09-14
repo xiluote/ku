51a894cc041502b9113ed6ece1b18d95be73b246
希洛特 <1210079813@qq.com>
2026-09-14 21:08:52 +0800

    9月14日，VQ模板匹配和窗体联动：硬币

---
+++ VQ/zuoye9yue14/zuoye9yue14/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace zuoye9yue14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(163, 231);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(660, 332);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "检测";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(350, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "正面硬币数量";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(568, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(350, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "一元硬币数量";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(350, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(180, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "五角硬币数量";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(350, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(180, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "一角硬币数量";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(568, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 28);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(568, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 28);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(568, 188);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 28);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 679);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ VQ/zuoye9yue14/zuoye9yue14/Form1.cs
@@ -0,0 +1,81 @@
+﻿using Cognex.VisionPro;
+using Cognex.VisionPro.ImageFile;
+using Cognex.VisionPro.ToolBlock;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zuoye9yue14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using (OpenFileDialog OFD = new OpenFileDialog())
+            {
+                OFD.Title = "请选择检测硬币数量的图片";
+                OFD.Filter = "图片|*.jpg;*.jpeg;*.png;*.gif;*.bmp;";
+                if (OFD.ShowDialog() == DialogResult.OK)
+                {
+                    //设置图片控件某些属性
+                    pictureBox1.Image = Image.FromFile(OFD.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    // 使用visionPro库处理图片，才能得到一张能被visionPro方案处理的图片
+                    CogImageFileTool CFT = new CogImageFileTool();
+                    CFT.Operator.Open(OFD.FileName, CogImageFileModeConstants.Read);
+                    CFT.Run();//运行
+
+                    //加载导出的vpp方案文件，绝对路径拼接
+                    string VppFilePath = Path.Combine(Application.StartupPath, "vpps", "zhengyingbi.vpp");
+                    string Vpp2 = Path.Combine(Application.StartupPath, "vpps", "meizhong.vpp");
+
+
+                    // 将vpp文件中的视觉方案读取到 C#中 
+                    object LOF = CogSerializer.LoadObjectFromFile(VppFilePath);
+                    object LOF2 = CogSerializer.LoadObjectFromFile(Vpp2);
+
+                    // 因为读取后 默认是 object 类型; 不具备Cog工具对象的运行方法,所以转给工具类型
+                    CogToolBlock CTB = LOF as CogToolBlock;
+                    CogToolBlock CTB2 = LOF2 as CogToolBlock;
+
+                    // 将CogImageFileTool工具读取到的输出图像 作为(视觉方案)CTB 的 输入图像
+                    CTB.Inputs["OutputImage"].Value = CFT.OutputImage;
+                    CTB.Run();
+                    CTB2.Inputs["OutputImage"].Value = CFT.OutputImage;
+                    CTB2.Run();
+
+
+                    // 获取数值并求和
+                    double yiyuan = Convert.ToDouble(CTB2.Outputs["yiyuan"].Value);
+                    double wujiao = Convert.ToDouble(CTB2.Outputs["wujiao"].Value);
+                    double yijiao = Convert.ToDouble(CTB2.Outputs["yijiao"].Value);
+
+                    double wuValue = Convert.ToDouble(CTB.Outputs["wu"].Value);
+                    double yiValue = Convert.ToDouble(CTB.Outputs["yi"].Value);
+                    double he = wuValue + yiValue;
+
+                    // 显示结果
+                    label2.Text = he.ToString();
+                    label6.Text = yiyuan.ToString();
+                    label7.Text = wujiao.ToString();
+                    label8.Text = yijiao.ToString();
+
+                }
+            }
+        }
+    }
+}
+
